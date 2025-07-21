6fe46e863c67bfb5066b5a6fe1b99bd8ef65a456
Kushika-Agarwal <kushikaagarwalg1080@gmail.com>
2025-07-21 23:19:49 +0530

    test 2

--- app/page.js
+++ app/page.js
@@ -39,50 +39,8 @@
 //   )
 // };
 
-"use client"; // Add this at the very top
+import ClientHome from "./components/ClientHome";
 
-import { useEffect, useState } from "react";
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-// import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
-
-export default function Home() {
-  const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const res = await fetch(
-          `https://dev.to/api/articles?username=${personalData.devUsername}`
-        );
-        const data = await res.json();
-        const filtered = data
-          .filter((item) => item?.cover_image)
-          .sort(() => Math.random() - 0.5);
-        setBlogs(filtered);
-      } catch (e) {
-        console.error("Failed to fetch blogs", e);
-      }
-    }
-    getData();
-  }, []);
-
-  return (
-    <div suppressHydrationWarning>
-      <HeroSection />
-      {/* <AboutSection />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education />
-      {/* <Blog blogs={blogs} /> 
-      <ContactSection /> */}
-    </div>
-  );
+export default function Page() {
+  return <ClientHome />;
 }
