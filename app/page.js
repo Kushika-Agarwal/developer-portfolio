62b103845fb5a031502bb78a0489047c464c7346
Kushika-Agarwal <kushikaagarwalg1080@gmail.com>
2025-07-21 23:10:30 +0530

    test 1

--- app/page.js
+++ app/page.js
@@ -44,7 +44,7 @@
 import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -76,13 +76,13 @@ export default function Home() {
   return (
     <div suppressHydrationWarning>
       <HeroSection />
-      <AboutSection />
+      {/* <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
-      <ContactSection />
+      {/* <Blog blogs={blogs} /> 
+      <ContactSection /> */}
     </div>
   );
 }
